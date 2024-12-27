7a74c0d3784da6273babe1568142e37e75d98f73
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2024-12-27 00:26:01 -0600

    Add decimal precision to tests

--- compatible.test.js
+++ compatible.test.js
@@ -1,5 +1,8 @@
 import fs from 'fs'
 import { LogicEngine, AsyncLogicEngine } from './index.js'
+import { configurePrecision } from './precision/index.js'
+import Decimal from 'decimal.js'
+
 const tests = []
 
 // get all json files from "suites" directory
@@ -13,141 +16,55 @@ for (const file of files) {
   }
 }
 
-// eslint-disable-next-line no-labels
-inline: {
-  const logic = new LogicEngine(undefined, { compatible: true })
-  const asyncLogic = new AsyncLogicEngine(undefined, { compatible: true })
-  const logicWithoutOptimization = new LogicEngine(undefined, { compatible: true })
-  const asyncLogicWithoutOptimization = new AsyncLogicEngine(undefined, { compatible: true })
+const engines = []
 
-  describe('All of the compatible tests', () => {
-    tests.forEach((testCase) => {
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )}`, () => {
-        expect(logic.run(testCase[0], testCase[1])).toStrictEqual(testCase[2])
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (async)`, async () => {
-        expect(await asyncLogic.run(testCase[0], testCase[1])).toStrictEqual(
-          testCase[2]
-        )
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (built)`, () => {
-        const f = logic.build(testCase[0])
-        expect(f(testCase[1])).toStrictEqual(testCase[2])
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (asyncBuilt)`, async () => {
-        const f = await asyncLogic.build(testCase[0])
-        expect(await f(testCase[1])).toStrictEqual(testCase[2])
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (noOptimization)`, () => {
-        expect(logicWithoutOptimization.run(testCase[0], testCase[1])).toStrictEqual(testCase[2])
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (asyncNoOptimization)`, async () => {
-        expect(await asyncLogicWithoutOptimization.run(testCase[0], testCase[1])).toStrictEqual(
-          testCase[2]
-        )
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (builtNoOptimization)`, () => {
-        const f = logicWithoutOptimization.build(testCase[0])
-        expect(f(testCase[1])).toStrictEqual(testCase[2])
-      })
-
-      test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-        testCase[1]
-      )} (asyncBuiltNoOptimization)`, async () => {
-        const f = await asyncLogicWithoutOptimization.build(testCase[0])
-        expect(await f(testCase[1])).toStrictEqual(testCase[2])
-      })
-    })
-  })
+for (let i = 0; i < 16; i++) {
+  let res = 'sync'
+  let engine = new LogicEngine(undefined, { compatible: true })
+  // sync / async
+  if (i & 1) {
+    engine = new AsyncLogicEngine(undefined, { compatible: true })
+    res = 'async'
+  }
+  // inline / disabled
+  if (i & 2) {
+    engine.disableInline = true
+    res += ' no-inline'
+  }
+  // optimized / not optimized
+  if (i & 4) {
+    engine.disableInterpretedOptimization = true
+    res += ' no-optimized'
+  }
+  // ieee754 / decimal
+  if (i & 8) {
+    configurePrecision(engine, Decimal)
+    res += ' decimal'
+  }
+  engines.push([engine, res])
 }
-// eslint-disable-next-line no-labels
-notInline: {
-  const logic = new LogicEngine(undefined, { compatible: true })
-  const asyncLogic = new AsyncLogicEngine(undefined, { compatible: true })
-  const logicWithoutOptimization = new LogicEngine(undefined, { compatible: true })
-  const asyncLogicWithoutOptimization = new AsyncLogicEngine(undefined, { compatible: true })
 
-  logicWithoutOptimization.disableInline = true
-  logic.disableInline = true
-  asyncLogic.disableInline = true
-  asyncLogicWithoutOptimization.disableInline = true
+describe('All of the compatible tests', () => {
+  for (const testCase of tests) {
+    for (const engine of engines) {
+      test(`${engine[1]} ${JSON.stringify(testCase[0])} ${JSON.stringify(
+        testCase[1]
+      )}`, async () => {
+        let result = await engine[0].run(testCase[0], testCase[1])
+        if ((result || 0).toNumber) result = Number(result)
+        if (Array.isArray(result)) result = result.map(i => (i || 0).toNumber ? Number(i) : i)
+        expect(result).toStrictEqual(testCase[2])
+      })
 
-  // using a loop to disable the inline compilation mechanism.
-  tests.forEach((testCase) => {
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )}`, () => {
-      expect(logic.run(testCase[0], testCase[1])).toStrictEqual(testCase[2])
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (async)`, async () => {
-      expect(await asyncLogic.run(testCase[0], testCase[1])).toStrictEqual(
-        testCase[2]
-      )
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (built)`, () => {
-      const f = logic.build(testCase[0])
-      expect(f(testCase[1])).toStrictEqual(testCase[2])
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (asyncBuilt)`, async () => {
-      const f = await asyncLogic.build(testCase[0])
-      expect(await f(testCase[1])).toStrictEqual(testCase[2])
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (noOptimization)`, () => {
-      expect(logicWithoutOptimization.run(testCase[0], testCase[1])).toStrictEqual(testCase[2])
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (asyncNoOptimization)`, async () => {
-      expect(await asyncLogicWithoutOptimization.run(testCase[0], testCase[1])).toStrictEqual(
-        testCase[2]
-      )
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (builtNoOptimization)`, () => {
-      const f = logicWithoutOptimization.build(testCase[0])
-      expect(f(testCase[1])).toStrictEqual(testCase[2])
-    })
-
-    test(`${JSON.stringify(testCase[0])} ${JSON.stringify(
-      testCase[1]
-    )} (asyncBuiltNoOptimization)`, async () => {
-      const f = await asyncLogicWithoutOptimization.build(testCase[0])
-      expect(await f(testCase[1])).toStrictEqual(testCase[2])
-    })
-  })
-}
+      test(`${engine[1]} ${JSON.stringify(testCase[0])} ${JSON.stringify(
+        testCase[1]
+      )} (built)`, async () => {
+        const f = await engine[0].build(testCase[0])
+        let result = await f(testCase[1])
+        if ((result || 0).toNumber) result = Number(result)
+        if (Array.isArray(result)) result = result.map(i => i.toNumber ? Number(i) : i)
+        expect(result).toStrictEqual(testCase[2])
+      })
+    }
+  }
+})
