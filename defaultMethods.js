5d8ab2069d1d2c984fac619928391cf0dbcfe3a0
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2025-01-03 12:40:09 -0600

    Remove the legacy methods from the default methods javascript file

--- defaultMethods.js
+++ defaultMethods.js
@@ -7,7 +7,7 @@ import declareSync from './utilities/declareSync.js'
 import { build, buildString } from './compiler.js'
 import chainingSupported from './utilities/chainingSupported.js'
 import InvalidControlInput from './errors/InvalidControlInput.js'
-import { splitPathMemoized } from './utilities/splitPath.js'
+import legacyMethods from './legacy.js'
 
 function isDeterministic (method, engine, buildState) {
   if (Array.isArray(method)) {
@@ -278,26 +278,6 @@ const defaultMethods = {
     if (i && typeof i === 'object') return Object.keys(i).length
     return 0
   },
-  get: {
-    method: ([data, key, defaultValue], context, above, engine) => {
-      const notFound = defaultValue === undefined ? null : defaultValue
-
-      const subProps = splitPathMemoized(String(key))
-      for (let i = 0; i < subProps.length; i++) {
-        if (data === null || data === undefined) {
-          return notFound
-        }
-        // Descending into context
-        data = data[subProps[i]]
-        if (data === undefined) {
-          return notFound
-        }
-      }
-      if (engine.allowFunctions || typeof data[key] !== 'function') {
-        return data
-      }
-    }
-  },
   exists: {
     method: (key, context, above, engine) => {
       const result = defaultMethods.val.method(key, context, above, engine, Unfound)
@@ -382,64 +362,6 @@ const defaultMethods = {
         return wrapNull(buildState.compile`(${res})`)
       }
       return false
-    }
-  },
-  var: (key, context, above, engine) => {
-    let b
-    if (Array.isArray(key)) {
-      b = key[1]
-      key = key[0]
-    }
-    let iter = 0
-    while (
-      typeof key === 'string' &&
-      key.startsWith('../') &&
-      iter < above.length
-    ) {
-      context = above[iter++]
-      key = key.substring(3)
-      // A performance optimization that allows you to pass the previous above array without spreading it as the last argument
-      if (iter === above.length && Array.isArray(context)) {
-        iter = 0
-        above = context
-        context = above[iter++]
-      }
-    }
-
-    const notFound = b === undefined ? null : b
-    if (typeof key === 'undefined' || key === '' || key === null) {
-      if (engine.allowFunctions || typeof context !== 'function') {
-        return context
-      }
-      return null
-    }
-    const subProps = splitPathMemoized(String(key))
-    for (let i = 0; i < subProps.length; i++) {
-      if (context === null || context === undefined) {
-        return notFound
-      }
-      // Descending into context
-      context = context[subProps[i]]
-      if (context === undefined) {
-        return notFound
-      }
-    }
-    if (engine.allowFunctions || typeof context !== 'function') {
-      return context
-    }
-    return null
-  },
-  missing: (checked, context, above, engine) => {
-    return (Array.isArray(checked) ? checked : [checked]).filter((key) => {
-      return defaultMethods.var(key, context, above, engine) === null
-    })
-  },
-  missing_some: ([needCount, options], context, above, engine) => {
-    const missing = defaultMethods.missing(options, context, above, engine)
-    if (options.length - missing.length >= needCount) {
-      return []
-    } else {
-      return missing
     }
   },
   map: createArrayIterativeMethod('map'),
@@ -745,16 +667,7 @@ Object.keys(defaultMethods).forEach((item) => {
       ? true
       : defaultMethods[item].deterministic
 })
-// @ts-ignore Allow custom attribute
-defaultMethods.var.deterministic = (data, buildState) => {
-  return buildState.insideIterator && !String(data).includes('../../')
-}
-Object.assign(defaultMethods.missing, {
-  deterministic: false
-})
-Object.assign(defaultMethods.missing_some, {
-  deterministic: false
-})
+
 // @ts-ignore Allow custom attribute
 defaultMethods['<'].compile = function (data, buildState) {
   if (!Array.isArray(data)) return false
@@ -926,81 +839,11 @@ defaultMethods['!!'].compile = function (data, buildState) {
   return `(!!engine.truthy(${data}))`
 }
 defaultMethods.none.deterministic = defaultMethods.some.deterministic
-defaultMethods.get.compile = function (data, buildState) {
-  let defaultValue = null
-  let key = data
-  let obj = null
-  if (Array.isArray(data) && data.length <= 3) {
-    obj = data[0]
-    key = data[1]
-    defaultValue = typeof data[2] === 'undefined' ? null : data[2]
-
-    // Bail out if the key is dynamic; dynamic keys are not really optimized by this block.
-    if (key && typeof key === 'object') return false
-
-    key = key.toString()
-    const pieces = splitPathMemoized(key)
-    if (!chainingSupported) {
-      return `(((a,b) => (typeof a === 'undefined' || a === null) ? b : a)(${pieces.reduce(
-        (text, i) => {
-          return `(${text}||0)[${JSON.stringify(i)}]`
-        },
-        `(${buildString(obj, buildState)}||0)`
-      )}, ${buildString(defaultValue, buildState)}))`
-    }
-    return `((${buildString(obj, buildState)})${pieces
-      .map((i) => `?.[${buildString(i, buildState)}]`)
-      .join('')} ?? ${buildString(defaultValue, buildState)})`
-  }
-  return false
-}
-// @ts-ignore Allow custom attribute
-defaultMethods.var.compile = function (data, buildState) {
-  let key = data
-  let defaultValue = null
-  if (
-    !key ||
-    typeof data === 'string' ||
-    typeof data === 'number' ||
-    (Array.isArray(data) && data.length <= 2)
-  ) {
-    if (Array.isArray(data)) {
-      key = data[0]
-      defaultValue = typeof data[1] === 'undefined' ? null : data[1]
-    }
-
-    if (key === '../index' && buildState.iteratorCompile) return 'index'
-
-    // this counts the number of var accesses to determine if they're all just using this override.
-    // this allows for a small optimization :)
-    if (typeof key === 'undefined' || key === null || key === '') return 'context'
-    if (typeof key !== 'string' && typeof key !== 'number') return false
-
-    key = key.toString()
-    if (key.includes('../')) return false
-
-    const pieces = splitPathMemoized(key)
-
-    if (!buildState.engine.allowFunctions) buildState.methods.preventFunctions = a => typeof a === 'function' ? null : a
-    else buildState.methods.preventFunctions = a => a
-
-    // support older versions of node
-    if (!chainingSupported) {
-      return `(methods.preventFunctions(((a,b) => (typeof a === 'undefined' || a === null) ? b : a)(${pieces.reduce(
-        (text, i) => `(${text}||0)[${JSON.stringify(i)}]`,
-        '(context||0)'
-      )}, ${buildString(defaultValue, buildState)})))`
-    }
-    return `(methods.preventFunctions(context${pieces
-      .map((i) => `?.[${JSON.stringify(i)}]`)
-      .join('')} ?? ${buildString(defaultValue, buildState)}))`
-  }
-  return false
-}
 
 // @ts-ignore Allowing a optimizeUnary attribute that can be used for performance optimizations
-defaultMethods['+'].optimizeUnary = defaultMethods['-'].optimizeUnary = defaultMethods.var.optimizeUnary = defaultMethods['!'].optimizeUnary = defaultMethods['!!'].optimizeUnary = defaultMethods.cat.optimizeUnary = true
+defaultMethods['+'].optimizeUnary = defaultMethods['-'].optimizeUnary = defaultMethods['!'].optimizeUnary = defaultMethods['!!'].optimizeUnary = defaultMethods.cat.optimizeUnary = true
 
 export default {
-  ...defaultMethods
+  ...defaultMethods,
+  ...legacyMethods
 }
---
+++ legacy.js
@@ -0,0 +1,156 @@
+import { buildString } from './compiler.js'
+import { splitPathMemoized } from './utilities/splitPath.js'
+import chainingSupported from './utilities/chainingSupported.js'
+import { Sync } from './constants.js'
+
+const legacyMethods = {
+  get: {
+    [Sync]: true,
+    method: ([data, key, defaultValue], context, above, engine) => {
+      const notFound = defaultValue === undefined ? null : defaultValue
+
+      const subProps = splitPathMemoized(String(key))
+      for (let i = 0; i < subProps.length; i++) {
+        if (data === null || data === undefined) return notFound
+        // Descending into context
+        data = data[subProps[i]]
+        if (data === undefined) return notFound
+      }
+
+      if (engine.allowFunctions || typeof data[key] !== 'function') return data
+      return null
+    },
+    deterministic: true,
+    compile: (data, buildState) => {
+      let defaultValue = null
+      let key = data
+      let obj = null
+      if (Array.isArray(data) && data.length <= 3) {
+        obj = data[0]
+        key = data[1]
+        defaultValue = typeof data[2] === 'undefined' ? null : data[2]
+
+        // Bail out if the key is dynamic; dynamic keys are not really optimized by this block.
+        if (key && typeof key === 'object') return false
+
+        key = key.toString()
+        const pieces = splitPathMemoized(key)
+        if (!chainingSupported) {
+          return `(((a,b) => (typeof a === 'undefined' || a === null) ? b : a)(${pieces.reduce(
+                (text, i) => `(${text}||0)[${JSON.stringify(i)}]`,
+                `(${buildString(obj, buildState)}||0)`
+              )}, ${buildString(defaultValue, buildState)}))`
+        }
+        return `((${buildString(obj, buildState)})${pieces
+              .map((i) => `?.[${buildString(i, buildState)}]`)
+              .join('')} ?? ${buildString(defaultValue, buildState)})`
+      }
+      return false
+    }
+  },
+  var: {
+    [Sync]: true,
+    method: (key, context, above, engine) => {
+      let b
+      if (Array.isArray(key)) {
+        b = key[1]
+        key = key[0]
+      }
+      let iter = 0
+      while (typeof key === 'string' && key.startsWith('../') && iter < above.length) {
+        context = above[iter++]
+        key = key.substring(3)
+        // A performance optimization that allows you to pass the previous above array without spreading it as the last argument
+        if (iter === above.length && Array.isArray(context)) {
+          iter = 0
+          above = context
+          context = above[iter++]
+        }
+      }
+
+      const notFound = b === undefined ? null : b
+      if (typeof key === 'undefined' || key === '' || key === null) {
+        if (engine.allowFunctions || typeof context !== 'function') return context
+        return null
+      }
+      const subProps = splitPathMemoized(String(key))
+      for (let i = 0; i < subProps.length; i++) {
+        if (context === null || context === undefined) return notFound
+
+        // Descending into context
+        context = context[subProps[i]]
+        if (context === undefined) return notFound
+      }
+
+      if (engine.allowFunctions || typeof context !== 'function') return context
+      return null
+    },
+    deterministic: (data, buildState) => buildState.insideIterator && !String(data).includes('../../'),
+    optimizeUnary: true,
+    compile: (data, buildState) => {
+      let key = data
+      let defaultValue = null
+      if (
+        !key ||
+          typeof data === 'string' ||
+          typeof data === 'number' ||
+          (Array.isArray(data) && data.length <= 2)
+      ) {
+        if (Array.isArray(data)) {
+          key = data[0]
+          defaultValue = typeof data[1] === 'undefined' ? null : data[1]
+        }
+
+        if (key === '../index' && buildState.iteratorCompile) return 'index'
+
+        // this counts the number of var accesses to determine if they're all just using this override.
+        // this allows for a small optimization :)
+        if (typeof key === 'undefined' || key === null || key === '') return 'context'
+        if (typeof key !== 'string' && typeof key !== 'number') return false
+
+        key = key.toString()
+        if (key.includes('../')) return false
+
+        const pieces = splitPathMemoized(key)
+
+        if (!buildState.engine.allowFunctions) buildState.methods.preventFunctions = a => typeof a === 'function' ? null : a
+        else buildState.methods.preventFunctions = a => a
+
+        // support older versions of node
+        if (!chainingSupported) {
+          return `(methods.preventFunctions(((a,b) => (typeof a === 'undefined' || a === null) ? b : a)(${pieces.reduce(
+              (text, i) => `(${text}||0)[${JSON.stringify(i)}]`,
+              '(context||0)'
+            )}, ${buildString(defaultValue, buildState)})))`
+        }
+        return `(methods.preventFunctions(context${pieces
+            .map((i) => `?.[${JSON.stringify(i)}]`)
+            .join('')} ?? ${buildString(defaultValue, buildState)}))`
+      }
+      return false
+    }
+  },
+  missing: {
+    [Sync]: true,
+    method: (checked, context, above, engine) => {
+      return (Array.isArray(checked) ? checked : [checked]).filter((key) => {
+        return legacyMethods.var.method(key, context, above, engine) === null
+      })
+    },
+    deterministic: false
+  },
+  missing_some: {
+    [Sync]: true,
+    method: ([needCount, options], context, above, engine) => {
+      const missing = legacyMethods.missing.method(options, context, above, engine)
+      if (options.length - missing.length >= needCount) {
+        return []
+      } else {
+        return missing
+      }
+    },
+    deterministic: false
+  }
+}
+
+export default legacyMethods
